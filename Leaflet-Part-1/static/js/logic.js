1cb5029fd263f7c77adc272a20f7dfdb0f35c1f7
amydst <amy.dst@gmail.com>
2024-11-21 16:50:11 -0800

    finished cleaning it up

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,146 +1,21 @@
-
-
-// // "USGS All Earthquakes, Past Hour" url
-// const quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-
-// // Creating map object:
-// // Longitude, latitude, and starting zoom level.
-// let map = L.map("map", {
-//     center: [10.60, -14.60],
-//     zoom: 2
-// });
-
-// L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-//     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-// }).addTo(map);
-
-// function markerSize(magnitude) {
-//     return (magnitude) * 100;
-// }
-
-// function createMarkers(response) {
-//     var quake = response.features;
-//     var quakeMarkers = [];
-
-//     for (var index = 0; index < quake.length; index++) {
-//         var earthquake = quake[index];
-
-//         // Create a marker with the correct coordinates
-//         var marker = L.marker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]])
-//             .bindPopup("<h3>" + earthquake.properties.place + "<br>Magnitude: " + earthquake.properties.mag + "</h3>");
-
-//         quakeMarkers.push(marker);
-//     }
-//     createMap(L.layerGroup(quakeMarkers));
-// }
-
-// d3.json(quakesUrl)
-//     .then(function(data) {
-//         createFeatures(data.features); // Call createFeatures with the fetched data
-//         createMarkers(data); // Call createMarkers with the fetched data
-//     });
-
-// function createFeatures(quakeData) {
-//     var quakeMarkers = []; // Create an array to hold the circle markers
-
-//     quakeData.forEach(function(feature) {
-//         var location = feature.properties.place;
-//         var coords = feature.geometry.coordinates;
-//         var magnitude = feature.properties.mag;
-//         var depth = coords[2];
-
-//         // Determine the color based on depth
-//         var color = depth > 150 ? 'darkred' : depth > 120 ? 'crimson' : depth > 90 ? 'mediumvioletred' : depth > 60 ? 'orchid' : depth > 30 ? 'plum': depth > 15 ? 'pink':'mistyrose';
-
-//         // Create a circle marker for each earthquake
-//         var circleMarker = L.circleMarker([coords[1], coords[0]], {
-//             radius: magnitude ** 2, // Scale the size
-//             fillColor: color,
-//             color: color,
-//             weight: 3,
-//             opacity: 1,
-//             fillOpacity: 0.75
-//         }).bindPopup('Location: ' + location +  '<br>Magnitude: ' + magnitude + '<br>Depth: ' + depth + ' km');
-
-//         quakeMarkers.push(circleMarker); // Add the circle marker to the array
-//     });
-
-//     // Add all circle markers to the map
-//     L.layerGroup(quakeMarkers).addTo(map);
-
-//     // Create a legend control
-//     var legend = L.control({position: 'bottomright'});
-
-//     legend.onAdd = function (map) {
-//       var div = L.DomUtil.create('div', 'info legend'),
-//           depths = [0, 15, 30, 60, 90, 120, 150], // Define depth intervals
-//           labels = [];
-
-//       // Loop through depth intervals and generate a label with a colored square
-//       for (var i = 0; i < depths.length; i++) {
-//           div.innerHTML +=
-//               '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' + // Use +1 to ensure the color reflects the next range
-//               depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
-//       }
-
-//       return div;
-//   };
-
-//   // Add the legend to the map
-//   legend.addTo(myMap);
-
-// }
-// // "USGS All Earthquakes, Past Hour" url
-
-// url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
-
-// d3.json(url).then(function(inforRes){
-
-// }
-// )
-
-// "USGS All Earthquakes, Past Hour" url
+// "USGS All Earthquakes, Past Day" url
 const quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
 // Creating map object:
-// Longitude, latitude, and starting zoom level.
+//  Longitude and latitude for center Kamsar, Guinea; and starting zoom level of 2.
 let map = L.map("map", {
     center: [10.60, -14.60],
     zoom: 2
 });
 
+//Tile layer with some topographic details
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
-function markerSize(magnitude) {
-    return (magnitude) * 100;
-}
-
-function createMarkers(response) {
-    var quake = response.features;
+//Function that designs all the features of the markers
+function createMarkerFeatures(quakeData) {
     var quakeMarkers = [];
-
-    for (var index = 0; index < quake.length; index++) {
-        var earthquake = quake[index];
-
-        // Create a marker with the correct coordinates
-        var marker = L.marker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]])
-            .bindPopup("<h3>" + earthquake.properties.place + "<br>Magnitude: " + earthquake.properties.mag + "</h3>");
-
-        quakeMarkers.push(marker);
-    }
-    createMap(L.layerGroup(quakeMarkers));
-}
-
-d3.json(quakesUrl)
-    .then(function(data) {
-        createFeatures(data.features); // Call createFeatures with the fetched data
-        createMarkers(data); // Call createMarkers with the fetched data
-    });
-
-function createFeatures(quakeData) {
-    var quakeMarkers = []; // Create an array to hold the circle markers
 
     quakeData.forEach(function(feature) {
         var location = feature.properties.place;
@@ -149,11 +24,11 @@ function createFeatures(quakeData) {
         var depth = coords[2];
 
         // Determine the color based on depth
-        var color = getColor(depth); // Use the getColor function to get the correct color for the legend
+        var color = getColor(depth);
 
         // Create a circle marker for each earthquake
         var circleMarker = L.circleMarker([coords[1], coords[0]], {
-            radius: magnitude ** 2, // Scale the size
+            radius: magnitude ** 2.2, // Scale the size
             fillColor: color,
             color: color,
             weight: 3,
@@ -170,13 +45,14 @@ function createFeatures(quakeData) {
     // Create a legend control
     var legend = L.control({position: 'bottomright'});
 
+    //Design the actual legend box
+
     legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend'),
-            depths = [0, 15, 30, 60, 90, 120, 150], // Define depth intervals
-            labels = [];
-        
-        
-        div.innerHTML = '<strong>Depth in km</strong><br>'; // Add title with bold text
+            depths = [0, 15, 30, 60, 90, 120, 150];
+                
+        div.innerHTML = '<strong>Depth in km</strong><br>';
+
         // Loop through depth intervals and generate a label with a colored square
         for (var i = 0; i < depths.length; i++) {
             div.innerHTML +=
@@ -185,15 +61,14 @@ function createFeatures(quakeData) {
         }
 
         // Add the background styling for the legend container
-        div.style.backgroundColor = 'rgba(255, 255, 255, 0.7)'; // Semi-transparent white background
-        div.style.padding = '10px'; // Padding inside the box
-        div.style.borderRadius = '5px'; // Rounded corners
-        div.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.2)'; // Shadow for better visibility
+        div.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+        div.style.padding = '10px';
+        div.style.borderRadius = '5px';
+        div.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.2)';
 
         return div;
     };
 
-    // Add the legend to the map
     legend.addTo(map);
 }
 
@@ -207,5 +82,12 @@ function getColor(depth) {
            depth > 15 ? 'pink' : 'mistyrose';
 }
 
+//Putting everything togeter to create the map and display all the eartquakes that have happened in the past day
+
+d3.json(quakesUrl)
+    .then(function(data) {
+        createMarkerFeatures(data.features);
+    }
+);
 
 
